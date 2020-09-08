36f3bbfbf52dad6f3dbf39b65a1c80154527e925
Niel Drummond <niel.drummond@fundingcircle.com>
2020-09-08 13:05:48 +0100

    Rewrite to put to /v2/groups

--- go.mod
+++ go.mod
@@ -1,6 +1,9 @@
-module github.com/drone-plugins/drone-marathon
+module github.com/FundingCircle/drone-marathon
+
+go 1.13
 
 require (
-	github.com/codegangsta/cli v1.20.0
-	github.com/joho/godotenv v1.2.0
+	github.com/drone-plugins/drone-marathon v0.0.0-20190331214941-0d050fd94875
+	github.com/joho/godotenv v1.3.0
+	github.com/urfave/cli v1.22.4
 )
--- marathon/client.go
+++ marathon/client.go
@@ -3,13 +3,13 @@ package marathon
 import (
 	"bytes"
 	"fmt"
+  "log"
 	"io/ioutil"
 	"net/http"
-	"text/template"
 )
 
 type Client interface {
-	GenerateApplicationDefinition() (string, error)
+	ReadMarathonFile() ([]byte, error)
 	CreateOrUpdateApplication() error
 }
 
@@ -21,68 +21,29 @@ func NewClient(params *Marathon) Client {
 	return &client{params}
 }
 
-func (c *client) GenerateApplicationDefinition() (string, error) {
-	c.extractProcessEnvKeys()
-	c.extractDockerParamsKeys()
-	c.normalizeDockerPortMappings()
-
-	tmpl, err := template.New("marathon").Parse(applicationDefinition)
-	if err != nil {
-		return "", err
-	}
-	buf := new(bytes.Buffer)
-	err = tmpl.Execute(buf, *c.params)
-	if err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
-
-func (c *client) extractProcessEnvKeys() {
-	c.params.ProcessEnvKeys = []string{}
-	for k := range c.params.ProcessEnv {
-		c.params.ProcessEnvKeys = append(c.params.ProcessEnvKeys, k)
-	}
-}
-
-func (c *client) extractDockerParamsKeys() {
-	c.params.DockerParamsKeys = []string{}
-	for k := range c.params.DockerParams {
-		c.params.DockerParamsKeys = append(c.params.DockerParamsKeys, k)
-	}
-}
-
-func (c *client) normalizeDockerPortMappings() {
-	for i, each := range c.params.DockerPortMappings {
-		if each.Protocol == "" {
-			c.params.DockerPortMappings[i].Protocol = "tcp"
-		}
-	}
+func (c *client) ReadMarathonFile() ([]byte, error) {
+  log.Printf("Params: %v", c.params)
+  data, err := ioutil.ReadFile(c.params.MarathonFile)
+  if err != nil {
+    return nil, err
+  }
+	return data, nil
 }
 
 func (c *client) CreateOrUpdateApplication() error {
-	appDefinition, err := c.GenerateApplicationDefinition()
+	groupDefinition, err := c.ReadMarathonFile()
 	if err != nil {
 		return err
 	}
 
 	if c.params.Debug {
-		fmt.Println("Request", appDefinition)
+		fmt.Println("Request", string(groupDefinition))
 	}
 
-	payload := []byte(appDefinition)
-
-	status, body, err := c.sendToServer("POST", "/v2/apps", payload)
-	if err != nil {
-		return err
-	}
-
-	if status == http.StatusConflict {
-		status, body, err = c.sendToServer("PUT", "/v2/apps/"+c.params.ID, payload)
-		if err != nil {
-			return err
-		}
-	}
+  status, body, err := c.sendToServer("PUT", "/v2/groups/", groupDefinition)
+  if err != nil {
+    return err
+  }
 
 	if c.params.Debug {
 		if err == nil {
@@ -101,9 +62,6 @@ func (c *client) CreateOrUpdateApplication() error {
 
 func (c *client) sendToServer(action, path string, definition []byte) (int, string, error) {
 	req, err := http.NewRequest(action, c.params.Server+path, bytes.NewBuffer(definition))
-	if c.params.Username != "" && c.params.Password != "" {
-		req.SetBasicAuth(c.params.Username, c.params.Password)
-	}
 	req.Header.Set("Content-Type", "application/json")
 	httpclient := &http.Client{}
 	resp, err := httpclient.Do(req)
